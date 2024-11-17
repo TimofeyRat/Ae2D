3a3165af49f6ea401b2ee7a7aca75dff82a7caab
Rul991 <archerartem6@gmail.com>
2024-11-17 13:47:41 +0300

    Add new methods to Vector

--- src/ae2d/math/Vector.rs
+++ src/ae2d/math/Vector.rs
@@ -2,14 +2,24 @@ use super::Point::Point;
 
 #[derive(Clone, Copy)]
 
-pub struct Vector { length: f64, angle: f64 }
+pub struct Vector 
+{ 
+	length: f64, 
+	angle: f64 
+}
 
 impl Vector
 {
+	pub fn new(&mut self, length: f64, angle: f64) -> Vector 
+	{
+		Vector {angle, length}
+	}
+
 	pub fn zero() -> Self
 	{
         Self { length: 0.0, angle: 0.0 }
     }
+
 	pub fn to_point(&mut self) -> Point
 	{
 		let a = f64::to_radians(self.angle);
@@ -18,5 +28,20 @@ impl Vector
 			x: f64::cos(a) * self.length,
 			y: f64::sin(a) * self.length
 		}
+	}
+
+	pub fn normalize(&mut self) -> Vector 
+	{
+		Vector {angle: self.angle, length: 1.0}
+	}
+
+	pub fn set_angle(&mut self, angle: f64) 
+	{
+		self.angle = angle;
+	}
+
+	pub fn set_length(&mut self, length: f64) 
+	{
+		self.length = length;
 	}
 }
